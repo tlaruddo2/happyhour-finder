9b0bc10f4f1115304cbe1ced96ee5429b62b3f4a
kukjinkim <tlaruddo2@gmail.com>
2023-09-10 20:48:17 -0700

    feat: add goole map
    - need to debug

--- src/components/restaurants-map/restautrants-map.tsx
+++ src/components/restaurants-map/restautrants-map.tsx
@@ -1,7 +1,15 @@
-const RestaurantsMap = () => {
+import { Wrapper } from "@googlemaps/react-wrapper";
+import GoogleMap from "./child/google-map";
+
+const RestaurantsMap = () => {    
+    const center = { lat: -34.397, lng: 150.644 };
+    const zoom = 8
+    
     return (
         <div>
-            Restaurant Map
+            <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY!}>
+                <GoogleMap center={center} zoom={zoom}/>
+            </Wrapper>
         </div>
     )
 }
