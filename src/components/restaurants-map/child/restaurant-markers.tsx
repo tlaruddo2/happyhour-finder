d9f8b8fbf10de736a54b55ba0f655785b4fe567b
kukjinkim <tlaruddo2@gmail.com>
2023-09-17 15:50:43 -0700

    feat: add infoBox of Marker in Google map - need to debug on clickHandler - need to decoate Box

--- src/components/restaurants-map/child/restaurant-markers.tsx
+++ src/components/restaurants-map/child/restaurant-markers.tsx
@@ -1,20 +1,45 @@
-import { MarkerF } from "@react-google-maps/api";
+import { MarkerF, InfoBoxF } from "@react-google-maps/api";
 import { restaurantSamples } from "entity/restaurant/restaurant"
+import { useState } from "react";
+import styled from "styled-components";
 
 const RestaurantMarkers = () => {
-    const clickHandler = () => console.log("marker clickcd")
+    const [ isOpend, setOpend ] = useState(false); 
+    const markerClickHandler = () => {
+        console.log("marker clickcd")
+        setOpend(true);
+    }
+    const closeClickHandler = () => {
+        console.log("clokse click")
+    }
     return (
         <>
             {restaurantSamples.map(( r ) =>
-                <MarkerF 
-                    key={r.id}
-                    onClick={clickHandler} 
-                    position={{ lat: r.lat, lng: r.lng }}
-                    icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-                />
+                <div key={r.id}>
+                    <MarkerF 
+                        onClick={markerClickHandler} 
+                        position={{ lat: r.lat, lng: r.lng }}
+                        icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+                    >
+                    </MarkerF>
+                    {isOpend &&
+                        <InfoBoxF position={new google.maps.LatLng(r.lat, r.lng)} onCloseClick={closeClickHandler}>
+                            <InfoBox>
+                                {r.detail}
+                            </InfoBox>
+                        </InfoBoxF>
+                    }
+                </div>
             )}    
         </>
     )
 }
+
+const InfoBox = styled.div`
+    width: 200px; 
+    height: 200px;
+    background-color: yellow;
+    padding: 2px;
+`
 
 export default RestaurantMarkers;
