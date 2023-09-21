30adfb28604c58e7a3c73ffde8257a801137f064
kukjinkim <tlaruddo2@gmail.com>
2023-09-20 23:33:48 -0700

    feat: add responsible InfoBox for ma

--- src/components/restaurants-map/child/restaurant-markers.tsx
+++ src/components/restaurants-map/child/restaurant-markers.tsx
@@ -1,36 +1,45 @@
 import { MarkerF, InfoBoxF } from "@react-google-maps/api";
 import { restaurantSamples } from "entity/restaurant/restaurant"
+import type { Restaurant } from "entity/restaurant/restaurant";
 import { useState } from "react";
 import styled from "styled-components";
 
 const RestaurantMarkers = () => {
-    const [ isOpend, setOpend ] = useState(false); 
-    const markerClickHandler = () => {
+    const [ isOpend, setOpend ] = useState(true); 
+    const [ detailed, setDetailed ] = useState("a");
+    const [ lat, setLat ] = useState(0);
+    const [ lng, setLng ] = useState(0);
+
+    const markerClickHandler = (samples: Restaurant[], n: number, ) => {
         console.log("marker clickcd")
         setOpend(true);
+        setDetailed(samples[n].detail);
+        setLat(samples[n].lat);
+        setLng(samples[n].lng);
+
     }
     const closeClickHandler = () => {
         console.log("clokse click")
     }
+
     return (
         <>
             {restaurantSamples.map(( r ) =>
-                <div key={r.id}>
-                    <MarkerF 
-                        onClick={markerClickHandler} 
-                        position={{ lat: r.lat, lng: r.lng }}
-                        icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-                    >
-                    </MarkerF>
-                    {isOpend &&
-                        <InfoBoxF position={new google.maps.LatLng(r.lat, r.lng)} onCloseClick={closeClickHandler}>
-                            <InfoBox>
-                                {r.detail}
-                            </InfoBox>
-                        </InfoBoxF>
-                    }
-                </div>
+                <MarkerF 
+                    onClick={() => markerClickHandler(restaurantSamples, r.id-1)} 
+                    position={{ lat: r.lat, lng: r.lng }}
+                    icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+                    key = {r.id}
+                >           
+                </MarkerF>
             )}    
+            {isOpend &&
+                <InfoBoxF position={new google.maps.LatLng(lat, lng)} onCloseClick={closeClickHandler}>
+                    <InfoBox>
+                        { detailed }
+                    </InfoBox>
+                </InfoBoxF>
+            }            
         </>
     )
 }
