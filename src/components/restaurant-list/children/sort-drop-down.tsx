baa92c04e11e35a2cd706e4419718a3ee80be4a3
kukjinkim <tlaruddo2@gmail.com>
2024-10-31 17:54:33 -0700

    feat/filter-restaurants: fix dropdown logic erorr

--- src/components/restaurant-list/children/sort-drop-down.tsx
+++ src/components/restaurant-list/children/sort-drop-down.tsx
@@ -1,6 +1,7 @@
 import { RestaurantType } from "state/types";
 import { Container } from "./styled/sort-drop-down.styled"
 import { RestaurantDetailedType } from "state/restaurants/restaurants-context";
+import React, { useRef } from 'react';
 
 interface Props{
     selectedRestaurantType: RestaurantType | 'All';
@@ -8,23 +9,37 @@ interface Props{
     setSelectedRestaurantDetailedType: React.Dispatch<React.SetStateAction<RestaurantDetailedType | 'All'>>
 }
 export const SortDropDown: React.FC<Props> = ({ selectedRestaurantType, setSelectedRestaurantType, setSelectedRestaurantDetailedType }) => {
-    const detailedPubType = ['Pub', 'CasualDining', 'FineDining', 'Seafood', 'SportsBar', 'BrewPub', 'Tapas', 'ViewBar', 'WineBar']
+    const selectRef = useRef<HTMLSelectElement>(null)
+    const detailedPubType = ['Pub', 'SportsBar', 'BrewPub', 'Tapas', 'ViewBar', 'WineBar']
     const detailedDiningType = ['CasualDining', 'FineDining', 'Seafood', 'Latin', 'Asian', 'Pizza']
+
+    const handleRestTypeChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedValue: string = e.target.value; 
+        setSelectedRestaurantType(selectedValue as RestaurantType | 'All')
+        setSelectedRestaurantDetailedType('All' as RestaurantDetailedType | 'All')
+        if (selectRef.current) {
+            selectRef.current.value = "all";
+        }
+    }
+
+    const handleRestDetailTypeChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedRestaurantDetailedType(e.target.value as RestaurantDetailedType | 'All')
+    }
 
     return (
         <Container>
             <div>
                 <label htmlFor="restaurantType">Pub or Dining</label>
-                <select name="restaurantType" onChange={(e) => setSelectedRestaurantType(e.target.value as RestaurantType | 'All')}>
+                <select name="restaurantType" onChange={handleRestTypeChanged}>
                     <option value="All">All</option>
-                    <option value="Pub">Pub</option>
+                    <option value="Pub">Pub</option>    
                     <option value="Dining">Dining</option>                
                 </select>
             </div>
             { selectedRestaurantType !== 'All' && 
             <div>
                 <label htmlFor="restaurantDetailedType">Select Types</label>
-                <select name="restaurantDetailedType" onChange={(e) => setSelectedRestaurantDetailedType(e.target.value as RestaurantDetailedType | 'All')}>
+                <select ref={selectRef} name="restaurantDetailedType" onChange={handleRestDetailTypeChanged}>
                     <option value="all">All</option>
                     {selectedRestaurantType === "Pub" ?
                     detailedPubType.map((t, i) => (
@@ -41,5 +56,3 @@ export const SortDropDown: React.FC<Props> = ({ selectedRestaurantType, setSelec
         </Container>
     )
 }
-
-// sort: distance, 
